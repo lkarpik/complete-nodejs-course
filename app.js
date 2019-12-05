50e79f15343af9c198560daf8e9e5c6bcc432145
Lukasz Karpik <lkarpik@wp.pl>
2019-12-05 13:05:41 +0100

    Cleaned up code from pug and handlebars

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-// const handlebars = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', handlebars({
-//     layoutsDir: 'views/layouts',
-//     defaultLayout: 'main',
-//     extname: 'hbs'
-// }));
-
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
-
 app.set('view engine', 'ejs');
-app.set('views', './views'); // default option is: ./views
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -30,7 +18,6 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {
         title: 'Page not found',
         path: false
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,14 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
+const router = express.Router();
 const adminData = require('./admin');
 
-const router = express.Router();
-
 router.get('/', (req, res, next) => {
-  console.log('Shop.js:', adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
   res.render('shop', {
     products: adminData.products,
     title: 'Shop',
-    hasProducts: adminData.products.length > 0,
-    path: '/',
-    activeShop: true,
-    productsCSS: true
+    path: '/'
   });
 });
 
