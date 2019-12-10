829f3daec7ed9ac254c9b3248b3cd3750213fad4
Lukasz Karpik <lkarpik@wp.pl>
2019-12-10 21:20:45 +0100

    Made constrains between products and users

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,13 +21,41 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err))
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    app.listen(3000);
-}).catch(err => {
-    console.log(err);
-})
+Product.belongsTo(User, {
+    constrains: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+sequelize.sync({
+        force: false
+    }).then(result => {
+        return User.findByPk(1);
+    }).then(user => {
+        if (!user) {
+            return User.create({
+                name: 'UKI',
+                email: 'uki@uki'
+            });
+        }
+        return user;
+    }).then(user => {
+        // console.log(user);
+        app.listen(3000);
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
