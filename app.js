4aa896d9caf84c4b2cc09f7298eca7c2a877493f
Lukasz Karpik <lkarpik@wp.pl>
2019-12-16 06:59:11 +0100

    mongodb connection

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database');
 
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+// const sequelize = require('./util/database');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/order-item');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -27,67 +28,73 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1).then(user => {
-        req.user = user;
-        next();
-    }).catch(err => console.log(err))
+    // User.findByPk(1).then(user => {
+    //     req.user = user;
+    //     next();
+    // }).catch(err => console.log(err))
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 // Constrains
-Product.belongsTo(User, {
-    constrains: true,
-    onDelete: 'CASCADE'
-});
-User.hasMany(Product);
-Cart.belongsTo(User);
-User.hasOne(Cart);
-Cart.belongsToMany(Product, {
-    through: CartItem
-});
-Product.belongsToMany(Cart, {
-    through: CartItem
-});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {
-    through: OrderItem
-});
+// Product.belongsTo(User, {
+//     constrains: true,
+//     onDelete: 'CASCADE'
+// });
+// User.hasMany(Product);
+// Cart.belongsTo(User);
+// User.hasOne(Cart);
+// Cart.belongsToMany(Product, {
+//     through: CartItem
+// });
+// Product.belongsToMany(Cart, {
+//     through: CartItem
+// });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, {
+//     through: OrderItem
+// });
 // OR:
 // Product.belongsToMany(Order, {
 //     through: OrderItem
 // });
 
 
-sequelize.sync({
-        force: false
-    }).then(result => {
-        return User.findByPk(1);
-    }).then(user => {
-        if (!user) {
-            return User.create({
-                name: 'UKI',
-                email: 'uki@uki'
-            });
-        }
-        return user;
-    })
-    .then(user => {
-        user.getCart().then(cart => {
-            if (!cart) {
-                return user.createCart();
-            } else {
-                return cart;
-            }
-        })
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+// sequelize.sync({
+//         force: false
+//     }).then(result => {
+//         return User.findByPk(1);
+//     }).then(user => {
+//         if (!user) {
+//             return User.create({
+//                 name: 'UKI',
+//                 email: 'uki@uki'
+//             });
+//         }
+//         return user;
+//     })
+//     .then(user => {
+//         user.getCart().then(cart => {
+//             if (!cart) {
+//                 return user.createCart();
+//             } else {
+//                 return cart;
+//             }
+//         })
+//     })
+//     .then(cart => {
+//         app.listen(3000);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const conf = require('../config');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '', {
-    host: 'localhost',
-    dialect: 'mariadb'
-});
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(conf.mongoDB).then(client => {
+        console.log('Connected to mongoDB');
+        callback(client);
+    }).catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
