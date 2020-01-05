c835d4b26c57e29e1a458672e16df67c427602ec
Lukasz Karpik <lkarpik@wp.pl>
2020-01-05 13:43:15 +0100

    Pagination done

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,15 +5,32 @@ const PDF = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numberOfProducts => {
+      totalItems = numberOfProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
